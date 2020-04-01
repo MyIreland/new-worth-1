38fef3e2722c36f76f403f6ec5ccbbecc6636a82
chenxiaoqing <chenxiaoqing@qianxx.com>
2020-04-01 15:44:25 +0800

    init

---
+++ src/main/java/cn/worth/WorthApplication.java
@@ -0,0 +1,39 @@
+package cn.worth;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//import org.activiti.spring.boot.SecurityAutoConfiguration;
+
+/**
+ * @Author: chenxiaoqing9
+ * @Date: Created in 2019/3/6
+ * @Description:
+ * @Modified by:
+ */
+@SpringBootApplication//(exclude = SecurityAutoConfiguration.class)//activiti启动报错解决方式 exclude = SecurityAutoConfiguration.class
+//@EnableScheduling
+@MapperScan({"cn.worth.**.mapper"})
+//@EnableSwagger2Doc
+public class WorthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorthApplication.class, args);
+
+        printHaveFun();
+    }
+
+
+    private static void printHaveFun() {
+        System.out.println(" ___  ___  ________  ___      ___ _______           ________ ___  ___  ________      \n" +
+                "|\\  \\|\\  \\|\\   __  \\|\\  \\    /  /|\\  ___ \\         |\\  _____\\\\  \\|\\  \\|\\   ___  \\    \n" +
+                "\\ \\  \\\\\\  \\ \\  \\|\\  \\ \\  \\  /  / | \\   __/|        \\ \\  \\__/\\ \\  \\\\\\  \\ \\  \\\\ \\  \\   \n" +
+                " \\ \\   __  \\ \\   __  \\ \\  \\/  / / \\ \\  \\_|/__       \\ \\   __\\\\ \\  \\\\\\  \\ \\  \\\\ \\  \\  \n" +
+                "  \\ \\  \\ \\  \\ \\  \\ \\  \\ \\    / /   \\ \\  \\_|\\ \\       \\ \\  \\_| \\ \\  \\\\\\  \\ \\  \\\\ \\  \\ \n" +
+                "   \\ \\__\\ \\__\\ \\__\\ \\__\\ \\__/ /     \\ \\_______\\       \\ \\__\\   \\ \\_______\\ \\__\\\\ \\__\\\n" +
+                "    \\|__|\\|__|\\|__|\\|__|\\|__|/       \\|_______|        \\|__|    \\|_______|\\|__| \\|__|\n" +
+                "                                                                                     \n" +
+                "                                                                                     ");
+    }
+}
--- src/main/java/cn/worth/file/config/OssProps.java
+++ src/main/java/cn/worth/file/config/OssProps.java
@@ -1,7 +1,8 @@
 package cn.worth.file.config;
 
-import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+//import lombok.Getter;
+//import lombok.Setter;
+//import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
  * @author myireland
@@ -9,32 +10,33 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * @date 2019-10-29
  * @description
  **/
-@Data
-@ConfigurationProperties(prefix = "aliyun.oss")
-public class OssProps {
-
-
-    /**
-     * accessKeyId
-     */
-    private String accessKeyId;
-    /**
-     *
-     */
-    private String accessKeySecret;
-
-    private String bucketName;
-
-    private String callback;
-    private String endpoint;
-    /**
-     * 内网
-     */
-    private String endpointInternal;
-
-    private long maxSize;
-    /**
-     * 代理过期时间 默认分钟
-     */
-    private long policyExpire;
-}
+//@Getter
+//@Setter
+//@ConfigurationProperties(prefix = "aliyun.oss")
+//public class OssProps {
+//
+//
+//    /**
+//     * accessKeyId
+//     */
+//    private String accessKeyId;
+//    /**
+//     *
+//     */
+//    private String accessKeySecret;
+//
+//    private String bucketName;
+//
+//    private String callback;
+//    private String endpoint;
+//    /**
+//     * 内网
+//     */
+//    private String endpointInternal;
+//
+//    private long maxSize;
+//    /**
+//     * 代理过期时间 默认分钟
+//     */
+//    private long policyExpire;
+//}
